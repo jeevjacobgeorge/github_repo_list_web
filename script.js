dcb799f8741dca2f4b2f33065271e7c0bfd609a6
jeevjacobgeorge <jeev@J4Hp>
2024-01-23 16:38:40 +0530

    Removed zone identifier file

--- script.js
+++ script.js
@@ -7,15 +7,18 @@ document.addEventListener("DOMContentLoaded", function() {
                     const card = document.createElement("div")
                     card.classList.add("card")
                     
-                    const repoName = document.createElement("p")
+                    const repoName = document.createElement("h3")
                     repoName.textContent = repo.name
-
+                    
+                    const repoLinkButton = document.createElement("button")
+                    repoLinkButton.classList.add("view-button")
                     const repoLink = document.createElement("a")
                     repoLink.href = repo.html_url
                     repoLink.textContent = "View on GitHub"
 
                     card.appendChild(repoName)  
-                    card.appendChild(repoLink)
+                    repoLinkButton.appendChild(repoLink)
+                    card.appendChild(repoLinkButton)
                     repoList.appendChild(card)
                 })
             })
